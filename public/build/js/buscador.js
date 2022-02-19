ada7dc597ad6ed575581ffb0078716928ec4b08c
Federico Aguilera <federico.aguilera@est.fi.uncoma.edu.ar>
2022-02-19 12:07:52 -0300

    build agregado

---
+++ public/build/js/buscador.js
@@ -0,0 +1,1 @@
+function iniciarApp(){window.location.href.includes("?fecha=")&&(location.href="#citas-admin"),buscarPorFecha()}function buscarPorFecha(){document.querySelector("#fecha").addEventListener("change",(function(n){const e=n.target.value;window.location="?fecha="+e}))}document.addEventListener("DOMContentLoaded",(function(){iniciarApp()}));
